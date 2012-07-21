a3c5b01f39ba8021999eb0475fd4f709d7426bca
Andrew Reed <areed@partitus.com>
2012-07-21 19:30:46 +0000

    added utility functions to DRY up API methods

--- mailchimp.go
+++ mailchimp.go
@@ -2,15 +2,15 @@ package mailchimp
 
 import (
 	"bytes"
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
+//	"log"
 	"net/http"
 	"net/url"
-	"os"
+//	"os"
 	"regexp"
+	"strconv"
 )
 
 var datacenter = regexp.MustCompile("[a-z]+[0-9]+$")
@@ -18,6 +18,14 @@ var datacenter = regexp.MustCompile("[a-z]+[0-9]+$")
 type api struct {
 	Key      string
 	endpoint string
+}
+
+type ChimpError struct {
+	Err string `json:"error"`
+	Code int
+}
+func (e ChimpError) Error() string {
+	return fmt.Sprintf("%v: %v", e.Code, e.Err)
 }
 
 func New(apikey string, https bool) (*api, error) {
@@ -29,14 +37,117 @@ func New(apikey string, https bool) (*api, error) {
 	}
 	u.Host = fmt.Sprintf("%s.api.mailchimp.com", datacenter.FindString(apikey))
 	u.Path = "/1.3/"
-	return &api{apikey, u.String()}, nil
+	return &api{apikey, u.String() + "?method="}, nil
+}
+
+func run(a *api, method string, parameters map[string]interface{}) ([]byte, error) {
+	if parameters == nil {
+		parameters = make(map[string]interface{})
+	}
+	parameters["apikey"] = a.Key
+	b, err := json.Marshal(parameters)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.Post(a.endpoint + method, "application/json", bytes.NewBuffer(b))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func verify(body []byte) error {
+	var e ChimpError
+	json.Unmarshal(body, &e)
+	if e.Err != "" || e.Code != 0 {
+		return e
+	}
+	return nil
+}
+
+func parseString(body []byte) (string, error) {
+	if err := verify(body); err != nil {
+		return "", err
+	}
+	return strconv.Unquote(string(body))
+}
+
+func parseStruct(body []byte, ret interface{}) error {
+	if err := verify(body); err != nil {
+		return err
+	}
+	json.Unmarshal(body, ret)
+	return nil
+}
+
+func parseBoolean(body []byte) (bool, error) {
+	if err := verify(body); err != nil {
+		return false, err
+	}
+	return strconv.ParseBool(string(body))
+}
+
+type CampaignContentResult struct {
+	Html string
+	Text string
+}
+func (a *api) CampaignContent(parameters map[string]interface{}) (*CampaignContentResult, error) {
+	body, err := run(a, "campaignContent", parameters)
+	if err != nil {
+		return nil, err
+	}
+	var ccr CampaignContentResult
+	err = parseStruct(body, &ccr)
+	return &ccr, nil
+}
+
+func (a *api) CampaignCreate(parameters map[string]interface{}) (string, error) {
+	body, err := run(a, "campaignCreate", parameters)
+	if err != nil {
+		return "", err
+	}
+	return parseString(body)
+}
+
+func (a *api) CampaignDelete(parameters map[string]interface{}) (bool, error) {
+	body, err := run (a, "campaignDelete", parameters)
+	if err != nil {
+		return false, err
+	}
+	return parseBoolean(body)
+}
+
+//not tested
+func (a *api) CampaignEcommOrderAdd(parameters map[string]interface{}) (bool, error) {
+	body, err := run (a, "campaignEcommOrderAdd", parameters)
+	if err != nil {
+		return false, err
+	}
+	return parseBoolean(body)
+}
+
+func (a *api) CampaignPause(parameters map[string]interface{}) (bool, error) {
+	body, err := run (a, "campaignPause", parameters)
+	if err != nil {
+		return false, err
+	}
+	return parseBoolean(body)
+}
+
+
+func (a *api) CampaignReplicate(parameters map[string]interface{}) (string, error) {
+	resp, err := run(a, "campaignReplicate", parameters)
+	if err != nil {
+		return "", err
+	}
+	return parseString(resp)
 }
 
 type CampaignsResult struct {
 	Total int
-	Data []CampaignsResultData
+	Data  []CampaignsResultData
 }
-
 type CampaignsResultData struct {
 	Id                string
 	Web_id            int
@@ -65,151 +176,38 @@ type CampaignsResultData struct {
 	Auto_footer       bool
 	Timewarp          bool
 	Timewarp_schedule string
-	Tracking CampaignsResultDataTracking
-	Segment_text string
-	Segment_opts []CampaignsResultDataSegment_opts
-	Type_opts []map[string]interface{}
+	Tracking          CampaignsResultDataTracking
+	Segment_text      string
+	Segment_opts      CampaignsResultDataSegment_opts
+	Type_opts         map[string]interface{}
 }
-
 type CampaignsResultDataTracking struct {
 	Html_clicks bool
 	Text_clicks bool
-	Opens bool
+	Opens       bool
 }
-
 type CampaignsResultDataSegment_opts struct {
-	Match string
+	Match      string
 	Conditions []map[string]interface{}
 }
-
 func (a *api) Campaigns(parameters map[string]interface{}) (*CampaignsResult, error) {
+	body, err := run(a, "campaigns", parameters)
+	var cr CampaignsResult
+	if err = parseStruct(body, &cr); err != nil {
+		return nil, err
+	}
+	return &cr, nil
 /*
-	//all passed in parameters are optional, but apikey is required
-	if parameters == nil {
-		parameters = make(map[string]interface{})
-	}
-	parameters["apikey"] = a.Key
-	b, err := json.Marshal(parameters)
-	if err != nil {
-		return nil, err
-	}
-	//don't modify a directly because it may be used by other methods
-	u := a.endpoint + "?method=campaigns"
-	resp, err := http.Post(u, "application/json", bytes.NewBuffer(b))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-*/
-//mock response for development
-	file, err := os.Open("/home/ec2-user/go/src/github.com/areed/mailchimp/campaigns.json")
+	//mock response for development
+	file, err := os.Open("/home/ec2-user/go/src/github.com/areed/mailchimp/go.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	reader := bufio.NewReader(file)
-	line, err := reader.ReadString('\n')
-	//create a temporary source map where the json blob can be unmarshalled and accessed;
-	//data will be taken from source map, type asserted, and placed in destination 
-	//struct that will be returned by Campaigns method
-	var sourceMap map[string]interface{}
-	if err = json.Unmarshal([]byte(line), &sourceMap); err != nil {
-		return nil, err
-	}
-	var result CampaignsResult
-	result.Total = int(sourceMap["total"].(float64))
-	//assert data in source map is a slice so we can loop over each item
-	data := sourceMap["data"].([]interface{})
-	for _, item := range data {
-		//assert each item in data is a map so it's accessible
-		datum := item.(map[string]interface{})
-		//the destination struct for this datum
-		var crd CampaignsResultData
-		//the form of type assertion that returns two values causes
-		//the first value to be the zero value for the type if the assertion
-		//fails, which it will if the value is nil (json null);
-		//type assertion of nil to string with a single return value
-		//would panic upon failure
-		crd.Id, _ = datum["id"].(string)
-		f64, _ := datum["web_id"].(float64)
-		crd.Web_id = int(f64)
-		crd.List_id, _ = datum["list_id"].(string)
-		f64, _ = datum["folder_id"].(float64)
-		crd.Folder_id = int(f64)
-		f64, _ = datum["template_id"].(float64)
-		crd.Template_id = int(f64)
-		crd.Content_type, _ = datum["content_type"].(string)
-		crd.Title, _ = datum["title"].(string)
-		crd.Type, _ = datum["type"].(string)
-		crd.Create_time, _ = datum["create_time"].(string)
-		crd.Send_time, _ = datum["send_time"].(string)
-		f64, _ = datum["emails_sent"].(float64)
-		crd.Emails_sent = int(f64)
-		crd.Status, _  = datum["status"].(string)
-		crd.From_name, _ = datum["from_name"].(string)
-		crd.From_email, _ = datum["from_email"].(string)
-		crd.Subject, _ = datum["subject"].(string)
-		crd.To_name, _ = datum["to_name"].(string)
-		crd.Archive_url, _ = datum["archive_url"].(string)
-		crd.Inline_css, _ = datum["inline_css"].(bool)
-		crd.Analytics, _ = datum["analytics"].(string)
-		crd.Analytics_tag, _ = datum["analytics_tag"].(string)
-		crd.Authenticate, _ = datum["authenticate"].(bool)
-		crd.Ecomm360, _ = datum["ecomm360"].(bool)
-		crd.Auto_tweet, _ = datum["auto_tweet"].(bool)
-		crd.Auto_fb_post, _ = datum["auto_fb_post"].(string)
-		crd.Auto_footer, _ = datum["auto_footer"].(bool)
-		crd.Timewarp, _ = datum["timewarp"].(bool)
-		crd.Timewarp_schedule, _  = datum["timewarp_schedule"].(string)
-		//populate tracking struct
-		//TODO: can probably directly do crd.Tracking.Html_clicks = tracking["html_clicks"].(bool)
-		tracking := datum["tracking"].(map[string]interface{})
-		var crdt CampaignsResultDataTracking
-		crdt.Html_clicks, _ = tracking["html_clicks"].(bool)
-		crdt.Text_clicks, _ = tracking["text_clicks"].(bool)
-		crdt.Opens, _ = tracking["opens"].(bool)
-		crd.Tracking = crdt
-		crd.Segment_text, _ = datum["segment_text"].(string)
-		//populate segment options array
-		segment_opts := datum["segment_opts"].([]interface{})
-		for _, item := range segment_opts {
-			segment_opt := item.(map[string]interface{})
-			var crds CampaignsResultDataSegment_opts
-			crds.Match = segment_opt["match"].(string)
-			conditions := segment_opt["conditions"].([]interface{})
-			for _, item := range conditions {
-				condition := item.(map[string]interface{})
-				crds.Conditions = append(crds.Conditions, condition)
-			}
-		}
-		//populate type options array
-		type_opts := datum["type_opts"].([]interface{})
-		for _, item := range type_opts {
-			type_opt := item.(map[string]interface{})
-			crd.Type_opts = append(crd.Type_opts, type_opt)
-		}
-		result.Data = append(result.Data, crd)
-	}
-	return &result, nil
+	body, err := ioutil.ReadAll(file)
+*/
 }
 
 func (a *api) Ping() (result string, err error) {
-	u := a.endpoint + "?method=ping"
-	b, err := json.Marshal(map[string]string{"apikey": a.Key})
-	if err != nil {
-		return "", err
-	}
-	resp, err := http.Post(u, "application/json", bytes.NewBuffer(b))
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(body), nil
+	body, err := run(a, "ping", make(map[string]interface{}))
+	return parseString(body)
 }
--- mailchimp_test.go
+++ mailchimp_test.go
@@ -5,15 +5,117 @@ import (
 	"testing"
 )
 
+var chimp, err = New(os.Getenv("MAILCHIMPKEY"), true)
+var cid = make(chan string, 1)
+
+/*
+func TestCampaignContent(t *testing.T) {
+	parameters := make(map[string]interface{})
+	parameters["cid"] = os.Getenv("MAILCHIMPCID")
+	parameters["for_archive"] = true
+	_, err = chimp.CampaignContent(parameters)
+	if err != nil {
+		t.Error("mailchimp.CampaignContent:", err)
+	}
+}
+*/
+
+/*
+func TestCampaignCreate(t *testing.T) {
+	parameters := make(map[string]interface{})
+	parameters["type"] = "regular"
+	options := make(map[string]interface{})
+	options["list_id"] = os.Getenv("MAILCHIMPLIST")
+	options["subject"] = "Go API test"
+	options["from_email"] = "support@partitus.com"
+	options["from_name"] = "Partitus"
+	options["to_name"] = "*|FNAME|*"
+	parameters["options"] = options
+	content := make(map[string]interface{})
+	content["html"] = "<p>Go API Test campaign html content</p>"
+	content["text"] = "Go API Test campaign text content"
+	parameters["content"] = content
+	id, err := chimp.CampaignCreate(parameters)
+	if err != nil {
+		t.Error("mailchimp.CampaignsCreate:", err)
+	}
+	//send to TestCampaignDelete
+	cid <- id
+}
+*/
+
+/*
+func TestCampaignDelete(t *testing.T) {
+	parameters := make(map[string]interface{})
+	//block until the cid for the campaign created in
+	//TestCampaignCreate is received
+	parameters["cid"] = <- cid
+	//TODO: figure out why this is happening
+	//for some reason double quotes are part of the string
+	//and the actual 10 digit id needs to be extracted
+	parameters["cid"] = parameters["cid"].(string)[1:11]
+	_, err := chimp.CampaignDelete(parameters)
+	if err != nil {
+		t.Error("mailchimp.CampaignsDelete:", err)
+		t.Error(parameters)
+	}
+}
+//*/
+
+func TestCampaignEcommOrderAdd(t *testing.T) {
+
+}
+
+/*
+func TestCampaignPause(t *testing.T) {
+	parameters := make(map[string]interface{})
+	parameters["cid"] = os.Getenv("MAILCHIMPRSS")
+	_, err = chimp.CampaignPause(parameters)
+	if err != nil {
+		if err.(ChimpError).Err != `Cannot pause this campaign because it is currently "paused"` {
+			t.Error(err)
+		}
+	}
+}
+*/
+
+/*
+func TestCampaignReplicate(t *testing.T) {
+	parameters := make(map[string]interface{})
+	parameters["cid"] = os.Getenv("MAILCHIMPCID")
+	_, err = chimp.CampaignReplicate(parameters)
+	if err != nil {
+		t.Error(err)
+	}
+}
+*/
+
+/*
 func TestCampaigns(t *testing.T) {
-	apikey := os.Getenv("MAILCHIMPKEY")
 	parameters := make(map[string]interface{})
 	filters := make(map[string]interface{})
 	filters["status"] = "sent"
 	parameters["filters"] = filters
-	api := API{"1.3", "campaigns", apikey, parameters}
-	_, err := api.Run()
+	result, err := chimp.Campaigns(parameters)
 	if err != nil {
-		t.Fail()
+		t.Error("mailchimp.Campaigns:", err)
+	}
+	if result.Data[0].Status != "sent" {
+		t.Error("mailchimp.Campaigns: json response did not properly unmarshal in CampaignsResult struct")
 	}
 }
+*/
+
+/*
+func TestPing(t *testing.T) {
+	result, err := chimp.Ping()
+	if err != nil {
+		t.Error("mailchimp.Ping", err)
+	}
+	if result != "Everything's Chimpy!" {
+		t.Error(`Expected response "Everything's Chimpy!" but received`, result)
+	}
+}
+*/
+
+
